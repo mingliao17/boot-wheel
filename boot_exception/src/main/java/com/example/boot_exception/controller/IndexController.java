23adddd612bc802fd21807ccdce9971f91ee9dc6
mingliao17 <ideal9527@gmail.com>
2021-08-19 23:52:09 +0800

    其他异常捕获

--- boot_exception/src/main/java/com/example/boot_exception/controller/IndexController.java
+++ boot_exception/src/main/java/com/example/boot_exception/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.boot_exception.controller;
 
+import com.example.boot_exception.common.BizException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +15,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
-    @GetMapping("/exception")
+    @GetMapping("/NullException")
     @ResponseBody
-    public String testException(){
+    public String NullException(){
         String str = null;
         str.toString();
         return  "test is ok!";
     }
+
+    @GetMapping("/BizException")
+    @ResponseBody
+    public String BizException() throws BizException {
+        if(1 == 1){//如果你能改变世界
+            throw new BizException("-1","这边是一个业务异常，自定义的反馈");
+        }
+        return  "test is ok!";
+    }
+
+    @GetMapping("/UnKnownException")
+    @ResponseBody
+    public String UnKnownException() {
+        Integer.valueOf("生活像一把无形刻刀");
+        return  "test is ok!";
+    }
+
+
 }
