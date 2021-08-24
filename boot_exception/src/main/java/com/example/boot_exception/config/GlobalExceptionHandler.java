e040676e9361b66a1b22fa5e54f2f2a62cd427e4
mingliao17 <ideal9527@gmail.com>
2021-08-24 11:52:50 +0800

    format

--- boot_exception/src/main/java/com/example/boot_exception/config/GlobalExceptionHandler.java
+++ boot_exception/src/main/java/com/example/boot_exception/config/GlobalExceptionHandler.java
@@ -25,41 +25,44 @@ public class GlobalExceptionHandler {
 
     /**
      * 处理自定义的业务异常
+     *
      * @param req
      * @param e
      * @return
      */
     @ExceptionHandler(value = BizException.class)
     @ResponseBody
-    public ResultBody bizExceptionHandler(HttpServletRequest req, BizException e){
-        log.error("发生业务异常！原因是：{}",e.getErrorMsg());
-        return ResultBody.error(e.getErrorCode(),e.getErrorMsg());
+    public ResultBody bizExceptionHandler(HttpServletRequest req, BizException e) {
+        log.error("发生业务异常！原因是：{}", e.getErrorMsg());
+        return ResultBody.error(e.getErrorCode(), e.getErrorMsg());
     }
 
     /**
      * 处理空指针的异常
+     *
      * @param req
      * @param e
      * @return
      */
-    @ExceptionHandler(value =NullPointerException.class)
+    @ExceptionHandler(value = NullPointerException.class)
     @ResponseBody
-    public ResultBody exceptionHandler(HttpServletRequest req, NullPointerException e){
-        log.error("发生空指针异常！原因是:",e);
+    public ResultBody exceptionHandler(HttpServletRequest req, NullPointerException e) {
+        log.error("发生空指针异常！原因是:", e);
         return ResultBody.error(CommonEnum.BODY_NOT_MATCH);
     }
 
 
     /**
      * 处理其他异常
+     *
      * @param req
      * @param e
      * @return
      */
-    @ExceptionHandler(value =Exception.class)
+    @ExceptionHandler(value = Exception.class)
     @ResponseBody
-    public ResultBody exceptionHandler(HttpServletRequest req, Exception e){
-        log.error("未知异常！原因是:",e);
+    public ResultBody exceptionHandler(HttpServletRequest req, Exception e) {
+        log.error("未知异常！原因是:", e);
         return ResultBody.error(CommonEnum.INTERNAL_SERVER_ERROR);
     }
 
