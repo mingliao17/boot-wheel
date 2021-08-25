cbbb44e31aa9f370adbac5591f31a0bd9862f018
mingliao17 <ideal9527@gmail.com>
2021-08-25 18:15:56 +0800

    自定义starter

--- mingliao-spring-boot-starter/src/main/java/MarkingAutoConfiguration.java
+++ mingliao-spring-boot-starter/src/main/java/MarkingAutoConfiguration.java
@@ -1,13 +1,24 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Random;
 
-@Configuration
+@Configuration//相当于一个普通的java配置类
+@ConditionalOnClass({MarkingService.class})// 当MarkingService 在类路径的条件下
+@EnableConfigurationProperties(MarkingProperties.class)// 将 application.properties 的相关的属性字段与该类一一对应，并生成 Bean
 public class MarkingAutoConfiguration {
 
+    @Autowired
+    private MarkingProperties markingProperties;
+
     @Bean
-    public Integer MarkingStringNums(String content){
-        return new Random(content.length()).nextInt();
+    @ConditionalOnMissingBean(MarkingService.class)
+    public MarkingService markingService(){
+        MarkingService markingService = new MarkingService();
+        return markingService;
     }
 }
